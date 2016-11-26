fa20dbc7354a12b823d4ffd30395e2ebab6c17be
Nick Mason <nick@thetuttroom.com>
2016-11-26 15:34:25 +0000

    more...

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-var keys = require('lib/keys.js');
-var cec  = require('lib/cec.js');
+var keys = require('./lib/keys.js');
+var cec  = require('./lib/cec.js');
 
 var KEY_NEXT   = 'n';
 var KEY_PREV   = 'p';
@@ -9,5 +9,11 @@ function next() {
   console.log('next');
 }
 
+function prev() {
+  console.log('prev');
+}
+
 keys.init();
 keys.registerKey(KEY_NEXT, next);
+keys.registerKey(KEY_PREV, prev);
+keys.registerKey(KEY_TOGGLE, cec.toggleActiveSource);
