151a64f67fac39804514334fe6a09598f4697017
Nick Mason <nick@thetuttroom.com>
2016-11-26 21:55:12 +0000

    drive

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
-var keys = require('./lib/keys.js');
-var cec  = require('./lib/cec.js');
-
-cec.connect();
+var keys  = require('./lib/keys.js');
+var cec   = require('./lib/cec.js');
+var drive = require('./lib/drive.js');
 
 var KEY_NEXT   = 'n';
 var KEY_PREV   = 'p';
 var KEY_TOGGLE = 't';
+
+cec.connect();
+drive.pull('./files', 5000);
+
+keys.init();
+keys.registerKey(KEY_NEXT, next);
+keys.registerKey(KEY_PREV, prev);
+keys.registerKey(KEY_TOGGLE, cec.toggleSelfActive);
 
 function next() {
   console.log('next');
@@ -14,8 +21,3 @@ function next() {
 function prev() {
   console.log('prev');
 }
-
-keys.init();
-keys.registerKey(KEY_NEXT, next);
-keys.registerKey(KEY_PREV, prev);
-keys.registerKey(KEY_TOGGLE, cec.toggleSelfActive);
