c5a25ce81f7078d9046df065c195bcb41e08fc8d
Nick Mason <nick@thetuttroom.com>
2016-11-26 21:36:19 +0000

    tidy up

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var keys = require('./lib/keys.js');
 var cec  = require('./lib/cec.js');
 
+cec.connect();
+
 var KEY_NEXT   = 'n';
 var KEY_PREV   = 'p';
 var KEY_TOGGLE = 't';
@@ -16,4 +18,4 @@ function prev() {
 keys.init();
 keys.registerKey(KEY_NEXT, next);
 keys.registerKey(KEY_PREV, prev);
-keys.registerKey(KEY_TOGGLE, cec.toggleActiveSource);
+keys.registerKey(KEY_TOGGLE, cec.toggleSelfActive);
